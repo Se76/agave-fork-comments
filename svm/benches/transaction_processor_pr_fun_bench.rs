bffc6e8cee86438225937d6b17c34cbd02003949
NickNut <68846529+Se76@users.noreply.github.com>
2025-01-27 20:03:40 +0100

    pew

--- svm/benches/transaction_processor_pr_fun_bench.rs
+++ svm/benches/transaction_processor_pr_fun_bench.rs
@@ -119,7 +119,7 @@ use {
         test_case::test_case,
     };
 
-use solana_program::feature;
+// use solana_program::feature;
 // use solana_program_runtime::loaded_programs::ForkGraph;
 // use solana_svm::transaction_processor_test::*;
 use {
@@ -149,12 +149,12 @@ impl ForkGraph for DummyForkGraph {
 }
 
 
-pub struct PayTubeAccountLoader<'a> {
+pub struct TestAccountLoader<'a> {
     cache: RwLock<HashMap<Pubkey, AccountSharedData>>,
     rpc_client: &'a RpcClient,
 }
 
-impl<'a> PayTubeAccountLoader<'a> {
+impl<'a> TestAccountLoader<'a> {
     pub fn new(rpc_client: &'a RpcClient) -> Self {
         Self {
             cache: RwLock::new(HashMap::new()),
@@ -169,7 +169,7 @@ impl<'a> PayTubeAccountLoader<'a> {
 /// ability to load accounts.
 ///
 /// In the Agave validator, this implementation is Bank, powered by AccountsDB.
-impl TransactionProcessingCallback for PayTubeAccountLoader<'_> {
+impl TransactionProcessingCallback for TestAccountLoader<'_> {
     fn get_account_shared_data(&self, pubkey: &Pubkey) -> Option<AccountSharedData> {
         if let Some(account) = self.cache.read().unwrap().get(pubkey) {
             return Some(account.clone());
@@ -189,75 +189,6 @@ impl TransactionProcessingCallback for PayTubeAccountLoader<'_> {
 
 
 
-// #[derive(Default, Clone)]
-//     pub struct MockBankCallback {
-//         pub account_shared_data: Arc<RwLock<HashMap<Pubkey, AccountSharedData>>>,
-//         #[allow(clippy::type_complexity)]
-//         pub inspected_accounts:
-//             Arc<RwLock<HashMap<Pubkey, Vec<(Option<AccountSharedData>, /* is_writable */ bool)>>>>,
-//     }
-
-//     impl TransactionProcessingCallback for MockBankCallback {
-//         fn account_matches_owners(&self, account: &Pubkey, owners: &[Pubkey]) -> Option<usize> {
-//             if let Some(data) = self.account_shared_data.read().unwrap().get(account) {
-//                 if data.lamports() == 0 {
-//                     None
-//                 } else {
-//                     owners.iter().position(|entry| data.owner() == entry)
-//                 }
-//             } else {
-//                 None
-//             }
-//         }
-
-//         fn get_account_shared_data(&self, pubkey: &Pubkey) -> Option<AccountSharedData> {
-//             self.account_shared_data
-//                 .read()
-//                 .unwrap()
-//                 .get(pubkey)
-//                 .cloned()
-//         }
-
-//         fn add_builtin_account(&self, name: &str, program_id: &Pubkey) {
-//             let mut account_data = AccountSharedData::default();
-//             account_data.set_data(name.as_bytes().to_vec());
-//             self.account_shared_data
-//                 .write()
-//                 .unwrap()
-//                 .insert(*program_id, account_data);
-//         }
-
-//         fn inspect_account(
-//             &self,
-//             address: &Pubkey,
-//             account_state: AccountState,
-//             is_writable: bool,
-//         ) {
-//             let account = match account_state {
-//                 AccountState::Dead => None,
-//                 AccountState::Alive(account) => Some(account.clone()),
-//             };
-//             self.inspected_accounts
-//                 .write()
-//                 .unwrap()
-//                 .entry(*address)
-//                 .or_default()
-//                 .push((account, is_writable));
-//         }
-//     }
-
-    // impl<'a> From<&'a MockBankCallback> for AccountLoader<'a, MockBankCallback> {
-    //     fn from(callbacks: &'a MockBankCallback) -> AccountLoader<'a, MockBankCallback> {
-    //         AccountLoader::new_with_account_cache_capacity(
-    //             None,
-    //             ProgramCacheForTxBatch::default(),
-    //             HashMap::default(),
-    //             callbacks,
-    //             Arc::<FeatureSet>::default(),
-    //             0,
-    //         )
-    //     }
-    // }
 
 
     pub struct TestAccountLoader<'a> {
@@ -339,3 +270,75 @@ fn my_benchmark<FG: ForkGraph>(c: &mut Criterion){
 criterion_group!(benches, my_benchmark<DummyForkGraph>);
 criterion_main!(benches);
 
+
+
+
+// #[derive(Default, Clone)]
+//     pub struct MockBankCallback {
+//         pub account_shared_data: Arc<RwLock<HashMap<Pubkey, AccountSharedData>>>,
+//         #[allow(clippy::type_complexity)]
+//         pub inspected_accounts:
+//             Arc<RwLock<HashMap<Pubkey, Vec<(Option<AccountSharedData>, /* is_writable */ bool)>>>>,
+//     }
+
+//     impl TransactionProcessingCallback for MockBankCallback {
+//         fn account_matches_owners(&self, account: &Pubkey, owners: &[Pubkey]) -> Option<usize> {
+//             if let Some(data) = self.account_shared_data.read().unwrap().get(account) {
+//                 if data.lamports() == 0 {
+//                     None
+//                 } else {
+//                     owners.iter().position(|entry| data.owner() == entry)
+//                 }
+//             } else {
+//                 None
+//             }
+//         }
+
+//         fn get_account_shared_data(&self, pubkey: &Pubkey) -> Option<AccountSharedData> {
+//             self.account_shared_data
+//                 .read()
+//                 .unwrap()
+//                 .get(pubkey)
+//                 .cloned()
+//         }
+
+//         fn add_builtin_account(&self, name: &str, program_id: &Pubkey) {
+//             let mut account_data = AccountSharedData::default();
+//             account_data.set_data(name.as_bytes().to_vec());
+//             self.account_shared_data
+//                 .write()
+//                 .unwrap()
+//                 .insert(*program_id, account_data);
+//         }
+
+//         fn inspect_account(
+//             &self,
+//             address: &Pubkey,
+//             account_state: AccountState,
+//             is_writable: bool,
+//         ) {
+//             let account = match account_state {
+//                 AccountState::Dead => None,
+//                 AccountState::Alive(account) => Some(account.clone()),
+//             };
+//             self.inspected_accounts
+//                 .write()
+//                 .unwrap()
+//                 .entry(*address)
+//                 .or_default()
+//                 .push((account, is_writable));
+//         }
+//     }
+
+    // impl<'a> From<&'a MockBankCallback> for AccountLoader<'a, MockBankCallback> {
+    //     fn from(callbacks: &'a MockBankCallback) -> AccountLoader<'a, MockBankCallback> {
+    //         AccountLoader::new_with_account_cache_capacity(
+    //             None,
+    //             ProgramCacheForTxBatch::default(),
+    //             HashMap::default(),
+    //             callbacks,
+    //             Arc::<FeatureSet>::default(),
+    //             0,
+    //         )
+    //     }
+    // }
